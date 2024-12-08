5e881e44a3151a58ae006f7935c08e3ba05ebd36
Simple <huyang1994822@gmail.com>
2024-12-08 22:11:22 +0800

    render list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
+import reactLogo from '/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const list = [
+    {id: 1, name: "Vue"},
+    {id: 2, name: "React"},
+]
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+    const [count, setCount] = useState(0);
+    const [person, setPerson] = useState({
+        name: "Simple",
+    });
+
+    const handleNameChange = (n: string) => {
+        setPerson({
+            ...person,
+            name: 'Hi, ' + n
+        })
+    }
+
+    return (
+        <>
+            <div>
+                <ul>
+                    {list.map((item, index) => (<li key={index}>{item.name}</li>))}
+                </ul>
+            </div>
+            <div>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <button onClick={() => handleNameChange(person.name)}>
+                    {person.name}
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default App
