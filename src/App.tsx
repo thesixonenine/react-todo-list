074ba7d9b33963f4bae7275d692493ed34a50739
Simple <huyang1994822@gmail.com>
2024-12-09 18:27:15 +0800

    update main.tsx App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import './App.css'
 import TodoItem from "./TodoItem.tsx";
 import Item from './structs/Item'
+import {ChangeEvent, useState} from "react";
 
-const todoList = [
-    {content: "React", color: 'green'},
-    {content: "Vue", color: 'blue'},
-]
 
 function App() {
+    const [todoList, setTodoList] = useState([
+        {id: 1, content: "React", color: 'green'},
+        {id: 2, content: "Vue", color: 'blue'},
+    ]);
+    const [value, setValue] = useState("");
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
 
     const itemClick = (it: Item) => {
         console.log(it.content)
+    }
+
+    const appendItem = () => {
+        if (value !== '') {
+            setTodoList([
+                ...todoList,
+                {
+                    id: todoList.map(t => t.id)
+                        .reduce((prev, cur) => Math.max(prev, cur)) + 1,
+                    content: value,
+                    color: 'green',
+                }
+            ])
+            console.log(todoList)
+        }
     }
 
     return (
@@ -18,19 +39,20 @@ function App() {
             <header>
                 <h1>Simple Todo List</h1>
             </header>
-
             <div className='list'>
                 <ul>
                     {
                         todoList.map(
-                            (item, index) =>
-                                <TodoItem key={index} item={item} click={itemClick}/>
+                            (item) =>
+                                <TodoItem key={item.id} item={item} click={itemClick}/>
                         )
                     }
                 </ul>
             </div>
             <div>
 
+                <input type='text' value={value} onChange={handleInputChange} placeholder='添加todo'/>
+                <button onClick={appendItem}>确定</button>
             </div>
         </>
     )
